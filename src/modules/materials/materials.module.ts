2fc0b3460e431c728205aaae11a5580b898c5b72
mrtsekar <mr.tsekar01@gmail.com>
2026-04-19 20:06:12 +0100

    fix: add LoggerProviderModule to MaterialsModule and QuotesModule for PINO_LOGGER dependency injection

--- src/modules/materials/materials.module.ts
+++ src/modules/materials/materials.module.ts
@@ -5,10 +5,12 @@ import { MaterialsService } from './service/materials.service';
 import { Quote } from '../quotes/entities/quote.entity';
 import { Material } from '../quotes/entities/material.entity';
 import { FileParserService } from '../../common/parsers/file-parser.service';
+import { LoggerProviderModule } from '../../common/modules/logger.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Quote, Material]),
+    LoggerProviderModule,
   ],
   controllers: [MaterialsController],
   providers: [MaterialsService, FileParserService],
